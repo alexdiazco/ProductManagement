dca4eecff19e22e407a69a923fcb3a910b875a61
alexdiaz <info@alexdiaz.co>
2021-04-01 18:59:41 -0500

    Implemented multiple reviews with arrays .

--- src/main/java/labs.pm.app/Shop.java
+++ src/main/java/labs.pm.app/Shop.java
@@ -15,36 +15,11 @@ public class Shop {
         Product p1 = pm.CreateProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
         pm.printProductReport();
         p1 = pm.reviewProduct(p1,Rating.FOUR_STAR,"Nice hot cup of tea");
+        p1 = pm.reviewProduct(p1,Rating.TWO_STAR,"oK");
+        p1 = pm.reviewProduct(p1,Rating.FOUR_STAR,"hot cup of tea");
+        p1 = pm.reviewProduct(p1,Rating.FOUR_STAR,"Nice hot");
+        p1 = pm.reviewProduct(p1,Rating.THREE_STAR," of tea");
+        p1 = pm.reviewProduct(p1,Rating.FIVE_STAR,"Nice hot cup ");
         pm.printProductReport();
-   /**
-        Product p2 = pm.CreateProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-        Product p3 = pm.CreateProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
-        Product p4 = pm.CreateProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now().plusDays(2));
-        Product p5 = p3.applyRating(Rating.THREE_STAR);
-
-        Product p6 = pm.CreateProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
-        Product p7 = pm.CreateProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
-
-        Product p8 = p4.applyRating(Rating.FIVE_STAR);
-        Product p9 = p1.applyRating(Rating.TWO_STAR);
-
-        // Comparing two object with override equal with different type and same id and name.
-        System.out.println(p6.equals(p7));
-
-        // IF getBestBefore is not in product.
-        //    if( p3 instanceof  Food){
-        //    LocalDate bestBefore = ((Food)p3).getBestBefore();
-        // }
-
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println(p4);
-        System.out.println(p5);
-        System.out.println(p6);
-        System.out.println(p7);
-        System.out.println(p8);
-        System.out.println(p9);
-    */
-    }
+     }
 }
